e46c24702ba6a74de5f08aaae5ba365ae843df1f
Paul Kuzhel <pavlo.kuzhel@gmail.com>
2015-04-23 17:55:02 -0400

    working version with slide and course control and status bar

--- library/createSimpleSlide.js
+++ library/createSimpleSlide.js
@@ -1,4 +1,5 @@
 // best structure to control slide content
+/*
 var slideContent = [
   { 
     title: "This is some title",
@@ -31,7 +32,7 @@ var slideContent = [
 var audios = [
 
 ];
-
+*/
 var AVIATION = AVIATION || {};
 
 // begin a javascript class "Slide"
@@ -117,6 +118,9 @@ AVIATION.common.Slide.prototype = {
     } else {
       this.buildContent();
     }
+
+    this.buildSlideControls();
+    this.buildCourseControls();
   },
 
   // method for building avatars into the slide
@@ -133,17 +137,80 @@ AVIATION.common.Slide.prototype = {
 
     var content = jQuery('<div/>', {
       "class": "cdot_contentText col-xs-12",
-      "html": "<h3>This is a title</h3><p>And some text</p>"
+      html: "<h3>This is a title</h3><p>And some text</p>"
     }).appendTo(this.container);
 
   },
 
+  // build controls that go immediately after the slide (play/pause buttons)
   buildSlideControls: function(){
+    var slideControlsRow = jQuery('<div/>', {
+      "class": "row",
+    }).appendTo(this.container);
+
+    this.insertLineBreak(slideControlsRow);
+
+    var slideControlsContainer = jQuery('<div/>', {
+      "id": "playerControls",
+    }).appendTo(slideControlsRow);
+
+    var slideControlsPrevious = jQuery('<div/>', {
+      "id": "btnPDiv",
+      html: '<a href="#" id="btnP" class="btn btn-default cdotBtn2" disabled role="button"></a>'
+    }).appendTo(slideControlsContainer);
+
+    var slideControlsPlay = jQuery('<div/>', {
+      "id": "btnPlayDiv",
+      html: '<a id="btnPlay" class="btn btn-default cdotBtn2" role="button"></a>' +
+            '<a id="btnPause" class="btn btn-default cdotBtn2" role="button"></a>' +
+            '<a id="btnR" class="btn btn-default cdotBtn2" role="button"></a>'
+    }).appendTo(slideControlsContainer);
+
+    var slideControlsNext = jQuery('<div/>', {
+      "id": "btnNDiv",
+      html: '<a href="#" id="btnN" class="btn btn-default cdotBtn2" disabled role="button"></a>'
+    }).appendTo(slideControlsContainer);
+
+    this.insertLineBreak(slideControlsRow);
+  },
+
+  insertLineBreak: function( element ){
+    try {
+      var lineBreak = jQuery('<div/>', {
+        "class": "col-xs-12",
+        html: "<!----><br/>"
+      }).appendTo(element);
+    } catch(error) {
+      console.log("error: " + error);
+      console.log("no element provided, line break not added");
+    }
+  },
+
+  insertSlideAudios: function(){
 
   },
 
+  // builds controls that go at the very bottom of the slide (back/continue) and status bar
   buildCourseControls: function(){
+    var courseControlsRow = jQuery('<div/>', {
+      "class": "row",
+    }).appendTo(this.container);
 
+    var statusBar = jQuery('<div/>', {
+      "class": "col-xs-12",
+      html: '<a href="#" id="cancelRedirect"' + 
+            'class="btn btn-default col-xs-offset-1 col-xs-10 col-sm-offset-2 col-sm-8 text-center" ' +
+            'role="button">Slide Loaded</a>'
+    }).appendTo(courseControlsRow);
+
+    this.insertLineBreak(courseControlsRow);
+
+    // TODO: make titles of buttons editable by allowing them to be a property inside options
+    var courseControlsContainer = jQuery('<div/>', {
+      "class": "col-xs-12",
+      html: '<a href="#" id="btnB" class="btn btn-default col-xs-6 col-sm-4" role="button">&lt; Back</a>' + 
+            '<a href="#" id="btnC" class="btn btn-default col-xs-6 col-sm-offset-4 col-sm-4" role="button">Continue &gt;</a>'
+    }).appendTo(courseControlsRow);
   },
   /*
   buildHighlights: function(){
