a20d760cc79b7d232e26baff45cd9b289311691e
Paul Kuzhel <pavlo.kuzhel@gmail.com>
2015-07-10 17:28:44 -0400

    a few css improvements

--- library/createSlide.js
+++ library/createSlide.js
@@ -514,6 +514,9 @@ AVIATION.common.Slide.prototype = {
     // console.log(this.options.isModal);
 
     if( !this.options.isModal && (!contentContainer || contentContainer.length === 0) ){
+
+      this.initSlider(activeIndex);
+
       contentContainer = jQuery('<div/>', {
         id: this.bodyId.split("#")[1],
         "class": this.options.showAvatars || this.options.enableSlider ? "cdot_contentText col-xs-8" : "cdot_contentText col-xs-12"
@@ -533,8 +536,6 @@ AVIATION.common.Slide.prototype = {
     // console.log(contentContainer);
     highlightsAddOnClick = this.initHighlights();
     this.buildHighlights(activeIndex, highlightsAddOnClick);
-
-    this.initSlider(activeIndex);
 
     buttonsAddOnClick = this.initButtons();
     console.log("buttons ADD ON CLICK!");
@@ -678,7 +679,7 @@ AVIATION.common.Slide.prototype = {
 
       footer = jQuery('<div/>', {
         id: this.options.footerId.split("#")[1],
-        class: this.options.isModal ? "modal-footer" : "row"
+        class: "slide_footer " + (this.options.isModal ? "modal-footer" : "row")
       });
 
       if(this.options.isModal){
@@ -1766,7 +1767,7 @@ AVIATION.common.Slide.prototype = {
       console.log("should it be? " + this.options.quizId);
       tempElement = jQuery("<div/>", {
         id: "quiz_" + i,
-        class: "cdot-quiz",
+        class: "cdot_quiz",
         html: "<h1 class='quizName'><!-- where the quiz name goes --></h1><div class='quizArea'><div class='quizHeader'><!-- where the quiz main copy goes --><a class='button startQuiz' href='#'>Get Started!</a></div><!-- where the quiz gets built --></div><div class='quizResults'><h3 class='quizScore'>You Scored: <span><!-- where the quiz score goes --></span></h3><h3 class='quizLevel'><!--<strong>Ranking:</strong>--> <span><!-- where the quiz ranking level goes --></span></h3><div class='quizResultsCopy'><!-- where the quiz result copy goes --></div></div>",
       }).appendTo(this.options.quizId);
       tempElement.slickQuiz(quizzes[i]);
